1654f72099c457a672be1ace9dd910425f0b005f
Georgios Kalanidis <georgios.kalanidis@elev.ga.ntig.se>
2024-09-06 14:40:43 +0200

    defrdxxgcfx

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Reflection.Metadata;
+
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.BackgroundColor = ConsoleColor.Cyan;
+Console.Clear();
+
+Console.WriteLine("Hur många bollar har du?");
+string bollar = Console.ReadLine();
+
+// Console.WriteLine("Hello "+ name + "!");
+
+
+
+Console.WriteLine($"Wow {bollar} is a lot of balls");
+
+Console.ReadLine();
+
+Console.WriteLine("Har du några bollar som är särskilt stora eller små, isåfall hur bred är den största du har??");
+
+string Bollar = Console.ReadLine();
+
+Console.WriteLine($"Wow {Bollar}, that is very cool");
+Console.ReadLine();
+
+Console.WriteLine("Och hur bred är ditt minsta boll??");
+
+string BOllar =  Console.ReadLine();
+
+Console.WriteLine($"{BOllar} is fascinating ");
+Console.ReadLine();
+
+Console.WriteLine("En sista fråga,hur mycket skiljer sig den från din största boll ??");
+
+string BOLlar = Console.ReadLine();
+
+Console.WriteLine($"{BOLlar} is a noticeable difference.");
+
+
+
+Console.ReadLine();
+
+Console.WriteLine($"You have a very intresting collection of different balls, like the amount of balls you have which was{bollar}, the different sizes such as {Bollar}, which was your biggest ball and {BOllar}, being your smallest ball. But lastly, the big difference between your biggest and smallest ball, which was {BOLlar} ");
+
+Console.ReadLine();
